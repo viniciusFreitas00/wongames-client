f6df35d2e6fdb885d812c703e0ec986048482df3
viniciusFreitas00 <vini.freirefreitas00@gmail.com>
2022-11-10 15:46:25 -0300

    Add MediaMatch responsive component

---
+++ src/components/MediaMatch/index.tsx
@@ -0,0 +1,28 @@
+import styled, { css } from 'styled-components'
+import media, { DefaultBreakpoints } from 'styled-media-query'
+
+type breakpoints = keyof DefaultBreakpoints
+
+export type MediaMatchProps = {
+  lessThan?: breakpoints
+  greaterThan?: breakpoints
+}
+
+const mediaMatchModifiers = {
+  lessThan: (size: breakpoints) => css`
+    ${media.lessThan(size)`display: block`}
+  `,
+
+  greaterThan: (size: breakpoints) => css`
+    ${media.greaterThan(size)`display: block`}
+  `
+}
+
+export const MediaMatch = styled.div<MediaMatchProps>`
+  ${({ lessThan, greaterThan }) => css`
+    display: none;
+
+    ${!!lessThan && mediaMatchModifiers.lessThan(lessThan)}
+    ${!!greaterThan && mediaMatchModifiers.greaterThan(greaterThan)}
+  `}
+`
