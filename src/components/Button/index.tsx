6bc62f8352ef54ff60e02955d8d291791345c4c5
viniciusFreitas00 <vini.freirefreitas00@gmail.com>
2022-12-11 12:27:16 -0300

    Add Button with sizes

---
+++ src/components/Button/index.tsx
@@ -0,0 +1,12 @@
+import * as S from './styles'
+
+export type ButtonProps = {
+  children?: React.ReactNode
+  size?: 'small' | 'medium' | 'large'
+}
+
+export function Button({ children, size = 'medium' }: ButtonProps) {
+  return (
+    <S.Wrapper size={size}>{!!children && <span>{children}</span>}</S.Wrapper>
+  )
+}
---
+++ src/components/Button/stories.tsx
@@ -0,0 +1,19 @@
+import { Story, Meta } from '@storybook/react/types-6-0'
+
+import { ButtonProps, Button } from '.'
+
+export default {
+  title: 'Button',
+  component: Button,
+  argTypes: {
+    children: {
+      type: 'string'
+    }
+  }
+} as Meta
+
+export const Default: Story<ButtonProps> = (args) => <Button {...args} />
+
+Default.args = {
+  children: 'Buy Now'
+}
