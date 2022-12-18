7c51171c119401456bb354dca43d6dc67c6d74f5
viniciusFreitas00 <vini.freirefreitas00@gmail.com>
2022-12-18 17:26:12 -0300

    Add Menu first tests and accessibility

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -9,7 +9,7 @@ export function Menu() {
   return (
     <S.Wrapper>
       <S.IconWrapper>
-        <MenuIcon />
+        <MenuIcon aria-label="Open Menu" />
       </S.IconWrapper>
 
       <S.LogoWrapper>
@@ -18,11 +18,11 @@ export function Menu() {
 
       <S.MenuGroup>
         <S.IconWrapper>
-          <SearchIcon />
+          <SearchIcon aria-label="Search" />
         </S.IconWrapper>
 
         <S.IconWrapper>
-          <ShoppingCartIcon />
+          <ShoppingCartIcon aria-label="Open Shopping cart" />
         </S.IconWrapper>
       </S.MenuGroup>
     </S.Wrapper>
