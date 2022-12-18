75952e46b72620de7852ad8a93ff8600339ef51b
viniciusFreitas00 <vini.freirefreitas00@gmail.com>
2022-12-18 19:26:31 -0300

    Add logic open/close to menu

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -1,14 +1,18 @@
+import { useState } from 'react'
 import { Menu2 as MenuIcon } from '@styled-icons/remix-fill/Menu2'
 import { ShoppingCart as ShoppingCartIcon } from '@styled-icons/material-outlined/ShoppingCart'
 import { Search as SearchIcon } from '@styled-icons/material-outlined/Search'
+import { Close as CloseIcon } from '@styled-icons/material-outlined/Close'
 
 import { Logo } from 'components/Logo'
 import * as S from './styles'
 
 export function Menu() {
+  const [isOpen, setIsOpen] = useState(false)
+
   return (
     <S.Wrapper>
-      <S.IconWrapper>
+      <S.IconWrapper onClick={() => setIsOpen(true)}>
         <MenuIcon aria-label="Open Menu" />
       </S.IconWrapper>
 
@@ -25,6 +29,10 @@ export function Menu() {
           <ShoppingCartIcon aria-label="Open Shopping cart" />
         </S.IconWrapper>
       </S.MenuGroup>
+
+      <S.MenuFull aria-hidden={!isOpen} isOpen={isOpen}>
+        <CloseIcon aria-label="Close Menu" onClick={() => setIsOpen(false)} />
+      </S.MenuFull>
     </S.Wrapper>
   )
 }
--- src/components/Menu/test.tsx
+++ src/components/Menu/test.tsx
@@ -1,4 +1,4 @@
-import { screen } from '@testing-library/react'
+import { fireEvent, screen } from '@testing-library/react'
 import { Menu } from '.'
 import { renderWithTheme } from 'utils/tests/helper'
 
@@ -10,5 +10,26 @@ describe('<Menu />', () => {
     expect(screen.getByRole('img', { name: /won games/i })).toBeInTheDocument()
     expect(screen.getByLabelText(/search/i)).toBeInTheDocument()
     expect(screen.getByLabelText(/open shopping cart/i)).toBeInTheDocument()
+  })
+
+  it('should handle the open/close mobile menu', () => {
+    renderWithTheme(<Menu />)
+
+    //selecionar menu full
+    const fullMenuElement = screen.getByRole('navigation', { hidden: true })
+
+    //verificar se o menu esta escondido
+    expect(fullMenuElement.getAttribute('aria-hidden')).toBe('true')
+    expect(fullMenuElement).toHaveStyle({ opacity: 0 })
+
+    //clicar no botão de menu e verificar se ele briu
+    fireEvent.click(screen.getByLabelText(/open menu/i))
+    expect(fullMenuElement.getAttribute('aria-hidden')).toBe('false')
+    expect(fullMenuElement).toHaveStyle({ opacity: 1 })
+
+    //clicar no botão de fechar e verificar se ele fechou
+    fireEvent.click(screen.getByLabelText(/close menu/i))
+    expect(fullMenuElement.getAttribute('aria-hidden')).toBe('true')
+    expect(fullMenuElement).toHaveStyle({ opacity: 0 })
   })
 })
