4933deb2f87155d5d0f7cbfd46ac246915b95e3d
viniciusFreitas00 <vini.freirefreitas00@gmail.com>
2022-12-19 22:33:27 -0300

    Ajuste links menu mobile

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -7,6 +7,7 @@ import { Close as CloseIcon } from '@styled-icons/material-outlined/Close'
 import * as S from './styles'
 import { Logo } from 'components/Logo'
 import { Button } from 'components/Button'
+import { MediaMatch } from 'components/MediaMatch'
 
 export type MenuProps = {
   username?: string
@@ -17,13 +18,22 @@ export function Menu({ username }: MenuProps) {
 
   return (
     <S.Wrapper>
-      <S.IconWrapper onClick={() => setIsOpen(true)}>
-        <MenuIcon aria-label="Open Menu" />
-      </S.IconWrapper>
+      <MediaMatch lessThan="medium">
+        <S.IconWrapper onClick={() => setIsOpen(true)}>
+          <MenuIcon aria-label="Open Menu" />
+        </S.IconWrapper>
+      </MediaMatch>
 
       <S.LogoWrapper>
         <Logo hideOnMobile />
       </S.LogoWrapper>
+
+      <MediaMatch greaterThan="medium">
+        <S.MenuNav>
+          <S.MenuLink href="#">Home</S.MenuLink>
+          <S.MenuLink href="#">Explorer</S.MenuLink>
+        </S.MenuNav>
+      </MediaMatch>
 
       <S.MenuGroup>
         <S.IconWrapper>
@@ -33,6 +43,12 @@ export function Menu({ username }: MenuProps) {
         <S.IconWrapper>
           <ShoppingCartIcon aria-label="Open Shopping cart" />
         </S.IconWrapper>
+
+        {!username && (
+          <MediaMatch greaterThan="medium">
+            <Button>Sing in</Button>
+          </MediaMatch>
+        )}
       </S.MenuGroup>
 
       <S.MenuFull aria-hidden={!isOpen} isOpen={isOpen}>
