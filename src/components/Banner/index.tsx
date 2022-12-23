1aecf8bfd2ce3979c6e8a661a0bd579828a0d538
viniciusFreitas00 <vini.freirefreitas00@gmail.com>
2022-12-22 22:30:38 -0300

    Add Banner structure

---
+++ src/components/Banner/index.tsx
@@ -0,0 +1,34 @@
+import { Button } from 'components/Button'
+import * as S from './styles'
+
+export type BannerProps = {
+  img: string
+  title: string
+  subtitle: string
+  buttonLabel: string
+  buttonLink: string
+}
+
+export function Banner({
+  img,
+  title,
+  subtitle,
+  buttonLabel,
+  buttonLink
+}: BannerProps) {
+  return (
+    <S.Wrapper>
+      <S.Image src={img} role="img" aria-label={title} />
+
+      <S.Caption>
+        <S.Title>{title}</S.Title>
+
+        <S.Subtitle dangerouslySetInnerHTML={{ __html: subtitle }} />
+
+        <Button as="a" href={buttonLink} size="large">
+          {buttonLabel}
+        </Button>
+      </S.Caption>
+    </S.Wrapper>
+  )
+}
---
+++ src/components/Banner/test.tsx
@@ -0,0 +1,11 @@
+import { render, screen } from '@testing-library/react'
+
+import { Banner } from '.'
+
+describe('<Banner />', () => {
+  it('should render the heading', () => {
+    //verique se o title existe renderizado
+    //verique se o subtitle existe renderizado
+    //verique se o imagem existe renderizado
+  })
+})
