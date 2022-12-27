d11302761e475af7a3e9660d5c7c049b1bee3a64
viniciusFreitas00 <vini.freirefreitas00@gmail.com>
2022-12-27 20:33:10 -0300

    Add promotionalPrice and test skeleton

--- src/components/GameCard/index.tsx
+++ src/components/GameCard/index.tsx
@@ -11,25 +11,38 @@ export type GameCardProps = {
   developer: string
   img: string
   price: string
+  promotionalPrice?: string
 }
 
-export function GameCard({ title, developer, img, price }: GameCardProps) {
+export function GameCard({
+  title,
+  developer,
+  img,
+  price,
+  promotionalPrice
+}: GameCardProps) {
   return (
     <S.Wrapper>
       <S.ImageBox>
         <img src={img} alt={title} />
       </S.ImageBox>
-      <S.Info>
-        <S.Title>{title}</S.Title>
-        <S.Developer>{developer}</S.Developer>
-      </S.Info>
-      <S.FavButton role="button">
-        <FavoriteBorder aria-label="Add to Wishlist" />
-      </S.FavButton>
-      <S.BuyBox>
-        <S.Price>{price}</S.Price>
-        <Button icon={<AddShoppingCart />} size="small" />
-      </S.BuyBox>
+
+      <S.Content>
+        <S.Info>
+          <S.Title>{title}</S.Title>
+          <S.Developer>{developer}</S.Developer>
+        </S.Info>
+
+        <S.FavButton role="button">
+          <FavoriteBorder aria-label="Add to Wishlist" />
+        </S.FavButton>
+
+        <S.BuyBox>
+          {!!promotionalPrice && <S.Price isPromotional>{price}</S.Price>}
+          <S.Price>{promotionalPrice || price}</S.Price>
+          <Button icon={<AddShoppingCart />} size="small" />
+        </S.BuyBox>
+      </S.Content>
     </S.Wrapper>
   )
 }
