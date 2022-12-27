2f23f6ea282f9bacb958b22fa746643bdfc274cc
viniciusFreitas00 <vini.freirefreitas00@gmail.com>
2022-12-26 21:02:38 -0300

    Add GameCard structure and initial tests

---
+++ src/components/GameCard/index.tsx
@@ -0,0 +1,35 @@
+import {
+  AddShoppingCart,
+  FavoriteBorder
+} from '@styled-icons/material-outlined'
+
+import * as S from './styles'
+import { Button } from 'components/Button'
+
+export type GameCardProps = {
+  title: string
+  developer: string
+  img: string
+  price: string
+}
+
+export function GameCard({ title, developer, img, price }: GameCardProps) {
+  return (
+    <S.Wrapper>
+      <S.ImageBox>
+        <img src={img} alt={title} />
+      </S.ImageBox>
+      <S.Info>
+        <S.Title>{title}</S.Title>
+        <S.Developer>{developer}</S.Developer>
+      </S.Info>
+      <S.FavButton role="button">
+        <FavoriteBorder aria-label="Add to Wishlist" />
+      </S.FavButton>
+      <S.BuyBox>
+        <S.Price>{price}</S.Price>
+        <Button icon={<AddShoppingCart />} size="small" />
+      </S.BuyBox>
+    </S.Wrapper>
+  )
+}
---
+++ src/components/GameCard/test.tsx
@@ -0,0 +1,32 @@
+import { screen } from '@testing-library/react'
+import { renderWithTheme } from 'utils/tests/helper'
+
+import { GameCard } from '.'
+
+const props = {
+  title: 'Population Zero',
+  developer: 'Rockstar Games',
+  img: 'https://source.unsplash.com/user/willianjusten/300x140',
+  price: 'R$ 235,00'
+}
+
+describe('<GameCard />', () => {
+  it('should render the heading', () => {
+    renderWithTheme(<GameCard {...props} />)
+
+    expect(
+      screen.getByRole('heading', { name: props.title })
+    ).toBeInTheDocument()
+
+    expect(
+      screen.getByRole('heading', { name: props.developer })
+    ).toBeInTheDocument()
+
+    expect(screen.getByRole('img', { name: props.title })).toHaveAttribute(
+      'src',
+      props.img
+    )
+
+    expect(screen.getByText(/235,00/i)).toBeInTheDocument()
+  })
+})
