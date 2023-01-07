66cb91b463aece00755cf62bc73ae6f963e6a24e
viniciusFreitas00 <vini.freirefreitas00@gmail.com>
2023-01-06 22:22:12 -0300

    Optimize Home tests

--- src/templates/Home/test.tsx
+++ src/templates/Home/test.tsx
@@ -11,13 +11,13 @@ import { Home } from '.'
 
 const props = {
   banners: bannersMock,
-  newGames: gamesMock,
+  newGames: [gamesMock[0]],
   mostPopularHighlight: highlightMock,
-  mostPopularGames: gamesMock,
-  upcommingGames: gamesMock,
+  mostPopularGames: [gamesMock[0]],
+  upcommingGames: [gamesMock[0]],
   upcommingHighlight: highlightMock,
-  upcommingMoreGames: gamesMock,
-  freeGames: gamesMock,
+  upcommingMoreGames: [gamesMock[0]],
+  freeGames: [gamesMock[0]],
   freeGamesHighlight: highlightMock
 }
 
@@ -44,7 +44,7 @@ describe('<Home />', () => {
     expect(
       screen.getByRole('heading', { name: /upcomming/i })
     ).toBeInTheDocument()
-
+    // logos (menu/footer)
     expect(
       screen.getByRole('heading', { name: /free games/i })
     ).toBeInTheDocument()
@@ -54,8 +54,8 @@ describe('<Home />', () => {
     renderWithTheme(<Home {...props} />)
     // banner
     expect(screen.getAllByText(/defy death 1/i)).toHaveLength(1)
-    // card game ( 5 sections com 4 cards cada = 5x4 = 20)
-    expect(screen.getAllByText(/population zero/i)).toHaveLength(20)
+    // card game ( 5 sections com 1 card cada = 5x1 = 5)
+    expect(screen.getAllByText(/population zero/i)).toHaveLength(5)
     // highlight
     expect(screen.getAllByText(/read dead it’s back/i)).toHaveLength(3)
   })
